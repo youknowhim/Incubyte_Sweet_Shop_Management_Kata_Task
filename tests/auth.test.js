340a84e50041415d3e8a366d71285dec4af66e5c
youknowhim <pallavrai39@gmail.com>
2025-12-13 17:30:32 +0530

    Included tests for normal users and admin users

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,7 +3,7 @@ const app = require("../server");
 
 describe("Auth APIs", () => {
 
-  it("should register a user", async () => {
+  it("should register a normal user", async () => {
     const res = await request(app)
       .post("/api/auth/register")
       .send({
@@ -16,12 +16,40 @@ describe("Auth APIs", () => {
     expect(res.statusCode).toBe(201);
   });
 
-  it("should login the user", async () => {
+
+  it("should register an admin user", async () => {
+    const res = await request(app)
+      .post("/api/auth/register")
+      .send({
+        name: "Admin",
+        email: "admin@example.com",
+        password: "admin123",
+        role: "admin"
+      });
+
+    expect(res.statusCode).toBe(201);
+  });
+
+
+  it("should login the normal user", async () => {
     const res = await request(app)
       .post("/api/auth/login")
       .send({
         email: "test@example.com",
         password: "123456"
+      });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+
+
+  it("should login the admin user", async () => {
+    const res = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: "admin@example.com",
+        password: "admin123"
       });
 
     expect(res.statusCode).toBe(200);
