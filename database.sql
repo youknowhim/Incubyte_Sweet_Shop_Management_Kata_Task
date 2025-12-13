bde6466e979a64240caf51cc81290a52537e7f65
youknowhim <pallavrai39@gmail.com>
2025-12-13 15:56:02 +0530

    example .env and db schema

---
+++ database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE sweetshop;
+
+USE sweetshop;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  role ENUM('user', 'admin') DEFAULT 'user'
+);
+
+CREATE TABLE sweets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL
+);
