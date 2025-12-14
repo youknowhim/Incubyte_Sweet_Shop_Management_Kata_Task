16d075d13d120575b1bb9175db3e9752df1a77e5
youknowhim <pallavrai39@gmail.com>
2025-12-15 01:02:03 +0530

    comma added

--- backend/database.sql
+++ backend/database.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
   name VARCHAR(255),
   email VARCHAR(255) UNIQUE,
   password VARCHAR(255),
-  role ENUM('user', 'admin') DEFAULT 'user'
+  role ENUM('user', 'admin') DEFAULT 'user',
   isTestUser BOOLEAN DEFAULT FALSE
 );
 
